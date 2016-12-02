f453d65829dc38c84d68f3780f725dff65da289e
Administrator <admin@local.host>
2016-12-02 19:01:54 +0200

    migrations and middleware

--- database/2016_12_02_165411_create_rooms_table.php
+++ database/2016_12_02_165411_create_rooms_table.php
@@ -15,6 +15,7 @@ class CreateRoomsTable extends Migration
     {
         Schema::create('rooms', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
